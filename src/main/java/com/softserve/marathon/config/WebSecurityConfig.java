5342fa71406bad6fc6a7de27f08cb494895c5cf2
Nick Underhill <m.podopryhora@gmail.com>
2020-08-07 17:31:29 +0300

    pre-merge

--- src/main/java/com/softserve/marathon/config/WebSecurityConfig.java
+++ src/main/java/com/softserve/marathon/config/WebSecurityConfig.java
@@ -1,47 +1,55 @@
 package com.softserve.marathon.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
+    @Qualifier("userServiceImpl")
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder(8);
     }
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("student").password(getPasswordEncoder().encode("student")).roles("USER")
-                .and()
-                .withUser("admin").password(getPasswordEncoder().encode("admin")).roles("ADMIN")
-                .and()
-                .withUser("mentor").password(getPasswordEncoder().encode("mentor")).roles("mentor");
-    }
 
     @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder());
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
                 .antMatchers("/index", "/", "/login**", "/registration**", "/css/*", "/static/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .loginProcessingUrl("/login")
+                .usernameParameter("email")
                 .defaultSuccessUrl("/marathons", true)
+                .permitAll()
                 .and()
                 .logout()
                 .logoutSuccessUrl("/login")
                 .deleteCookies("JSESSIONID");
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
     }
 }
